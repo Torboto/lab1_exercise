5791c34043f611e815e016c3458a2ebcf00a5ff3
Torboto <motifo@gmail.com>
2014-01-14 08:52:18 -0700

    Completed student class

--- student.py
+++ student.py
@@ -1,16 +1,28 @@
-class student:
+class Student:
     courseMarks = {}
     name = ""
 
     def __init__(self, name, family):
-        self.name = name
-    
+        self.name = '%s %s' % (name, family)
+
     def addCourseMark(self, course, mark):
         self.courseMarks[course] = mark
 
     def average(self):
-        int markSum
-        for mark in courseMarks:
-            markSum =+ mark
-        return markSum / courseMarks.len()
+        if len(self.courseMarks) > 0:
+            markSum = 0
+            for course in self.courseMarks:
+                markSum += self.courseMarks[course]
+            return markSum / len(self.courseMarks)
+        else:
+            print "No marks!"
+
+john = Student('John', 'Lame')
+print john.name
+john.addCourseMark('CMPUT410', 87)
+john.addCourseMark('CMPUT411', 97)
+john.addCourseMark('CMPUT412', 77)
+john.addCourseMark('CMPUT413', 76)
+john.addCourseMark('CMPUT414', 95)
+print "Average: %d" % john.average()
 
